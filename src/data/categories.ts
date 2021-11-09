e84de0af6852793ad4404ad1d8e521a0fce5b966
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2021-11-09 09:54:46 -0300

    finalizado

---
+++ src/data/categories.ts
@@ -0,0 +1,9 @@
+import {Category} from '../types/Category';
+
+export const categories: Category = {
+    extra: {title: 'Extra', color: 'lightGreen', expense: false},
+    fuel: {title: 'Gasolina', color: 'purple', expense: true},
+    food: {title: 'Alimentação', color: 'blue', expense: true},
+    rent: {title: 'Aluguel', color: 'brown', expense: true},
+    salary: {title: 'Salário', color: 'green', expense: false},
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+    {date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2021, 9, 15), category: 'food', title: 'BurguerKing', value: 28.99},
+    {date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apartamento', value: 1300.00},
+    {date: new Date(2021, 10, 18), category: 'salary', title: 'Salário programador', value: 3000.00},
+];
